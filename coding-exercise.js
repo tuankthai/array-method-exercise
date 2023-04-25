ba2686c8194222dfb164b2f38df10891de11f918
dsl2022 <2015rpro@gmail.com>
2023-04-24 20:26:43 -0400

    first commit

---
+++ coding-exercise.js
@@ -0,0 +1,31 @@
+// Prompt: Write a function that removes the last element of an array and returns it.
+function removeLastElement(arr) {
+    // TODO: Implement the logic here
+  }
+  
+// Prompt: Write a function that adds an element to the end of an array and returns the new length of the array.
+function addElementToEnd(arr, element) {
+    // TODO: Implement the logic here
+  }
+
+// Prompt: Write a function that removes the first element of an array and returns it.
+function removeFirstElement(arr) {
+    // TODO: Implement the logic here
+  }
+
+  
+// Prompt: Write a function that adds an element to the beginning of an array and returns the new length of the array.
+function addElementToBeginning(arr, element) {
+    // TODO: Implement the logic here
+  }
+  
+// Prompt: Write a function that returns a section of an array based on a start and end index.
+function getArraySection(arr, start, end) {
+    // TODO: Implement the logic here
+  }
+
+// Prompt: Write a function that removes a section of an array and returns the removed elements.
+function removeArraySection(arr, start, end) {
+    // TODO: Implement the logic here
+  }
+  
